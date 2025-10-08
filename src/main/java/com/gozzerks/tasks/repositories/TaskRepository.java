f70bfa6097cefb1758ccdf97e0b01663e1a7fbde
Vivaldodjol <v.djol@hotmail.com>
2025-10-08 12:31:19 +0100

    Implement: TaskRepository, TaskListRepository

---
+++ src/main/java/com/gozzerks/tasks/repositories/TaskRepository.java
@@ -0,0 +1,13 @@
+package com.gozzerks.tasks.repositories;
+
+import com.gozzerks.tasks.domain.entities.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface TaskRepository extends JpaRepository<Task, UUID> {
+    List<Task> findByTaskListId(UUID taskListId);
+    Optional<Task> findByTaskListIdAndId(UUID taskListId, UUID id);
+}
